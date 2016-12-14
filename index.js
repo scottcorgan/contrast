4301b90895992cfa7676e626b192700370929a37
Rob Scott <rob.scott@s4rb.com>
2016-12-14 10:20:16 +0000

    introduce configurable threshold for a more custom light/dark reporting

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var hexToRgb = require('hex-to-rgb');
 
-module.exports = function contrast (hex) {
+module.exports = function contrast (hex, threshold) {
+  if (typeof threshold === 'undefined') {
+    threshold = 180;
+  }
+
   var rgb = hexToRgb(hex);
   var o = Math.round(((parseInt(rgb[0]) * 299) + (parseInt(rgb[1]) * 587) + (parseInt(rgb[2]) * 114)) /1000);
 
-  return (o <= 180) ? 'dark' : 'light';
+  return (o <= threshold) ? 'dark' : 'light';
 };
--- test.js
+++ test.js
@@ -2,22 +2,31 @@ var contrast = require('./index');
 var test = require('tape');
 
 test('dark', function (t) {
-  
   t.equal(contrast('000000'), 'dark', 'black is dark');
   t.equal(contrast('666666'), 'dark', 'gray is dark');
   t.end();
 });
 
+test('dark with threshold', function (t) {
+  t.equal(contrast('000000', 255), 'dark', 'black with threshold of 255 is dark');
+  t.equal(contrast('666666', 100), 'light', 'gray with threshold of 100 is light');
+  t.end();
+});
+
 test('light', function (t) {
-  
   t.equal(contrast('ffffff'), 'light', 'white is light');
   t.equal(contrast('cccccc'), 'light', 'gray is light');
   t.end();
 });
 
-test('with hash', function (t) {
-  
-  t.equal(contrast('#000000'), 'dark', 'dark with hash');
+test('light with threshold', function (t) {
+  t.equal(contrast('ffffff', 255), 'dark', 'white with threshold of 0 is dark');
+  t.equal(contrast('cccccc', 210), 'dark', 'gray with threshold of 220 is dark');
+  t.equal(contrast('cccccc', 200), 'light', 'gray with threshold of 230 is light');
   t.end();
 });
 
+test('with hash', function (t) {
+  t.equal(contrast('#000000'), 'dark', 'dark with hash');
+  t.end();
+});
